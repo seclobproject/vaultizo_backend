965a811b191a10f37ab0fff0c971a5578e67a4f1
hornto <krishnadas10.official@gmail.com>
2024-06-22 17:28:34 +0530

    fea : set the refresh token in cookies and provide the access token refreshing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import User from "../models/user.js";
 import { validateRegister } from "../models/ValidationShema.js";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
 import { twilioClient, emailTransporter } from "../config/otpConfig.js";
 import { parsePhoneNumberFromString } from "libphonenumber-js";
 import otpVerification from "../models/otpModel.js";
@@ -48,7 +48,6 @@ export const register = async (req, res) => {
 export const sendOTPLoginVerification = async (req, res) => {
   try {
     const { email, phone, password } = req.body;
-    console.log(req.body);
 
     if (!email && !phone) {
       return res
@@ -111,48 +110,93 @@ export const sendOTPLoginVerification = async (req, res) => {
 export const OtpVerification = async (req, res) => {
   try {
     const otp = req.body.otp;
-    const userVerification = await otpVerification.findOne({ otp: otp });
 
-    if (!userVerification) {
-      return res.status(400).json({
-        status: "error",
-        message: "User verification failed or Otp expired ",
-      });
-    }
-    if (userVerification) {
+    if (otp) {
+      // OTP Verification Logic
+      const userVerification = await otpVerification.findOne({ otp: otp });
+      if (!userVerification) {
+        return res.status(400).json({
+          status: "error",
+          message: "User verification failed or Otp expired ",
+        });
+      }
+
       const user = await User.findOne({ email: userVerification.email });
 
-      const generateAccessToken = (user) => {
-        return jwt.sign(
-          { id: user.id, username: user.name },
-          process.env.USER_ACCESS_TOKEN_SECRET,
-          { expiresIn: "1d" }
-        );
-      };
+      if (!user) {
+        return res.status(400).json({
+          status: "error",
+          message: "User not found",
+        });
+      }
+      // Generating access token
 
-      // Function to generate refresh token
-      const generateRefreshToken = (user) => {
-        return jwt.sign(
-          { id: user.id, username: user.name },
-          process.env.REFRESH_TOKEN_SECRET,
-          { expiresIn: "7d" }
-        );
-      };
+      const accessToken = jwt.sign(
+        {
+          username: user.name,
+          email: user.email,
+        },
+        process.env.USER_ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "10m",
+        }
+      );
+      // Generating refresh token
+      const refreshToken = jwt.sign(
+        {
+          username: user.username,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
 
-      // Generate JWT access and refresh token
-      const accessToken = generateAccessToken(user);
-      const refreshToken = generateRefreshToken(user);
-      let refreshTokens = [];
-
-      // Store the refresh token securely in the empty array
-      refreshTokens.push(refreshToken);
-
-      // user information including the refresh and access tokens
-      res.status(200).json({
+      // Assigning refresh token in http-only cookie
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+      });
+      return res.status(200).json({
         status: "success",
         message: "Login successful",
-        data: { accessToken, refreshToken, username: user.name, id: user.id },
+        data: { accessToken, username: user.name, id: user.id },
       });
+    } else if (refreshToken) {
+      // verify refresh token
+      jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        async (err, decode) => {
+          if (err) {
+            return res.status(406).json({ message: "Unauthorized" });
+          }
+          const user = await User.findOne({ username: decode.name });
+
+          if (!user) {
+            return res.status(400).json({
+              status: "error",
+              message: "User not found",
+            });
+          }
+
+          const accessToken = jwt.sign(
+            {
+              username: user.name,
+              email: user.email,
+            },
+            process.env.USER_ACCESS_TOKEN_SECRET,
+            { expiresIn: "10m" }
+          );
+          return res.status(200).json({
+            status: "success",
+            message: "Token refreshed",
+            data: { accessToken, username: user.name, id: user._id },
+          });
+        }
+      );
+    } else {
+      return res.status(400).json({ message: "Bad request" });
     }
   } catch (error) {
     console.error(error);
@@ -274,42 +318,45 @@ export const AddPersonalDetails = async (req, res) => {
       accountCreationDate,
       vaultizoReferralCode,
     } = req.body;
-    const personaldetail = await User.find({_id : userId})
-    const data = personaldetail[0].personalDetails
-    if(data.length < 1){
-    await User.findOneAndUpdate(
-      { _id: userId },
-      {
-        $push: {
-          personalDetails: {
-            name: name,
-            nickname: nickname,
-            dateOfBirth: dateOfBirth,
-            gender: gender,
-            maritalStatus: maritalStatus,
-            idCardNo: idCardNo,
-            email: email,
-            mobileNumber: mobileNumber,
-            country: country,
-            address: address,
-            bankName: bankName,
-            branchName: branchName,
-            accountNo: accountNo,
-            vaultizoUserId: vaultizoUserId,
-            accountCreationDate: accountCreationDate,
-            vaultizoReferralCode: vaultizoReferralCode,
-          },
-        },
-      }
-    );
-    res
-      .status(200)
-      .json({ status: "success", message: "Successfully added the personal details" });
-  }res
-      .status(400)
-      .json({ status: "faliure", message: "already addaed the personal details" });
 
-    
+    const personaldetail = await User.find({ _id: userId });
+    const data = personaldetail[0].personalDetails;
+
+    if (data.length < 1) {
+      await User.findOneAndUpdate(
+        { _id: userId },
+        {
+          $push: {
+            personalDetails: {
+              name: name,
+              nickname: nickname,
+              dateOfBirth: dateOfBirth,
+              gender: gender,
+              maritalStatus: maritalStatus,
+              idCardNo: idCardNo,
+              email: email,
+              mobileNumber: mobileNumber,
+              country: country,
+              address: address,
+              bankName: bankName,
+              branchName: branchName,
+              accountNo: accountNo,
+              vaultizoUserId: vaultizoUserId,
+              accountCreationDate: accountCreationDate,
+              vaultizoReferralCode: vaultizoReferralCode,
+            },
+          },
+        }
+      );
+      res.status(200).json({
+        status: "success",
+        message: "Successfully added the personal details",
+      });
+    }
+    res.status(400).json({
+      status: "faliure",
+      message: "already addaed the personal details",
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: "error", message: "Server error" });
@@ -325,3 +372,60 @@ export default {
   ChangePassword,
   AddPersonalDetails,
 };
+
+//       const generateAccessToken = (user) => {
+//         return jwt.sign(
+//           { id: user.id, username: user.name },
+//           process.env.USER_ACCESS_TOKEN_SECRET,
+//           { expiresIn: "1d" }
+//         );
+//       };
+
+//       // Function to generate refresh token
+//       const generateRefreshToken = (user) => {
+//         return jwt.sign(
+//           { id: user.id, username: user.name },
+//           process.env.REFRESH_TOKEN_SECRET,
+//           { expiresIn: "7d" }
+//         );
+//       };
+
+//       // Generate JWT access and refresh token
+//       const accessToken = generateAccessToken(user);
+//       const refreshToken = generateRefreshToken(user);
+//       let refreshTokens = [];
+
+//       // Store the refresh token securely in the empty array
+//       refreshTokens.push(refreshToken);
+
+//       // user information including the refresh and access tokens
+//       res.status(200).json({
+//         status: "success",
+//         message: "Login successful",
+//         data: { accessToken, refreshToken, username: user.name, id: user.id },
+//       });
+//     }
+
+// export const RefreshAccesstoken = async (req,res) => {
+//   if (req.cookies?.jwt){
+//   //destructure the refresh token from the cookies
+//   const refreshToken = req.cookies.jwt
+
+//   jwt.verify(refreshToken ,process.env.REFRESH_TOKEN_SECRET,
+//     (err,decode) => {
+//       if(err){
+
+//        // Wrong Refesh Token
+//         return res.status(406).json({ message: 'Unauthorized' });
+//       }
+//       else {
+//       // sending the correct refresh token
+//       const accessToken = jwt.sign({
+//         username :
+//       })
+//       }
+//     }
+//    )
+
+//   }
+// }
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import './dotenv.js'
 import userRoute from './routes/UserRoute.js';
 import express from 'express';
-import mongoose from './config/dbConnection.js'; 
+import mongoose from './config/dbConnection.js';
+import cookieparser from "cookie-parser"  
 
 const app = express();
 const port = process.env.PORT || 3000 ;
 
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieparser());
 app.use('/user',userRoute)
 
 app.listen(port, () => {
